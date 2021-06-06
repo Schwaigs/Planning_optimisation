5f783e6059b07732d636763f3301e82d9797349c
Schwaigs <sarah.schwaiger.cha@gmail.com>
2021-06-07 01:27:45 +0200

    adding windows scripts

---
+++ buildWindows.bat
@@ -0,0 +1,7 @@
+cd src
+g++    -c -o main.o main.cpp
+g++    -c -o ae.o ae.cpp
+g++    -c -o chromosome.o chromosome.cpp
+g++    -c -o population.o population.cpp
+cc    -c -o instancesFormations/instance-80formations_5.o instancesFormations/instance-80formations_5.c
+g++ -o main main.o ae.o chromosome.o population.o instancesFormations/instance-80formations_5.o -L/lib -L/usr/lib -L/usr/local/lib -lm

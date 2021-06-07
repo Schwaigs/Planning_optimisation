39667235116ed649394c8fd050fcc99f512c4976
ElMitsuki <tommyramos9876@gmail.com>
2021-06-07 16:06:32 +0200

    modif du buildWindows pour laisser la fenetre ouverte

--- buildWindows.bat
+++ buildWindows.bat
@@ -5,3 +5,4 @@ g++    -c -o chromosome.o chromosome.cpp
 g++    -c -o population.o population.cpp
 gcc    -c -o instancesFormations/instance-80formations_5.o instancesFormations/instance-80formations_5.c
 g++ -o main main.o ae.o chromosome.o population.o instancesFormations/instance-80formations_5.o -L/lib -L/usr/lib -L/usr/local/lib -lm
+cmd /k

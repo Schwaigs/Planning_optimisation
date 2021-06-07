12a1158ee5921cd5e83f4849b53f5e66474fe60a
Schwaigs <sarah.schwaiger.cha@gmail.com>
2021-06-07 16:53:28 +0200

    update linux run script and information displayed

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,10 @@ int main(int argc, char **argv)
 // argc : nombre de parametres
 // argv : tableau contenant les parametres
 
-// Soit l'executable 'algo_evo' ne prend pas d'arguments soit il prend 4 arguments :
+// Soit l'executable 'main' ne prend pas d'arguments soit il prend 3 arguments :
 //    1. taille de la population (entier > 0)
 //    2. taux de croisement (0 <= reel <= 1)
 //    3. taux de mutation (0 <= reel <= 1)
-//    4. nombre de formations (=taille d'un chromosome)
 {
 	//initialise le g�n�rateur de nombre al�atoire
 	Random::randomize();
@@ -28,12 +27,12 @@ int main(int argc, char **argv)
 	float taux_mutation   = 0.01;
 	int taille_chromosome = NBR_FORMATIONS;
 
-	if (argc == 5)
+	if (argc == 4)
 	{
 		taille_population = atoi(argv[1]);
 		taux_croisement   = atof(argv[2]);
 		taux_mutation     = atof(argv[3]);
-		taille_chromosome = atoi(argv[4]);
+		//la taille d'un chromosome n'est pas modifiable
 	}
 	else if (argc == 1)
 	{
@@ -50,6 +49,10 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	cout << "La taille de la population est de " << taille_population << endl;
+	cout << "Le taux de croisement est de " << taux_croisement << endl;
+	cout << "Le taux de mutation est de " << taux_mutation << endl;
+	cout << "Il y a " << NBR_APPRENANTS << " apprenants suivant chacun " << NBR_FORMATIONS_APPRENANT << " formation(s). La taille d'un chromosome est donc de " << taille_chromosome << endl << endl;
 
 	// initialise l'algorithme �volutionniste
 	Ae algo(taille_population, taux_croisement, taux_mutation, taille_chromosome);
